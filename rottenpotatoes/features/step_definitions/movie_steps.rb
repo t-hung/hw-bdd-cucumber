465136f6c02d0561931409599bd3d92def7af66b
Teddy Hung <thung@sandiego.edu>
2018-10-07 21:51:17 +0000

    updated

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -4,8 +4,8 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create!(movie)
   end
-  fail "Unimplemented"
 end
 
 Then /(.*) seed movies should exist/ do | n_seeds |
@@ -18,6 +18,7 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
+  
   fail "Unimplemented"
 end
 
@@ -29,7 +30,14 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+     rating_list.split do |curRating|
+         if uncheck
+             uncheck(curRating)
+         else 
+             check(curRating)
+         end
+     end
+
 end
 
 Then /I should see all the movies/ do
--- rottenpotatoes/features/step_definitions/web_steps.rb
+++ rottenpotatoes/features/step_definitions/web_steps.rb
@@ -121,11 +121,12 @@ Then /^(?:|I )should see \/([^\/]*)\/$/ do |regexp|
 end
 
 Then /^(?:|I )should not see "([^"]*)"$/ do |text|
-  if page.respond_to? :should
-    page.should have_no_content(text)
-  else
-    assert page.has_no_content?(text)
-  end
+    expect(page).to have_no_content(text)
+ # if page.respond_to? :should
+  #  page.should have_no_content(text)
+  #else
+  #  assert page.has_no_content?(text)
+  #end
 end
 
 Then /^(?:|I )should not see \/([^\/]*)\/$/ do |regexp|
